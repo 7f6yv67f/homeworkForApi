157def50878ca39ad44d370ba7c0dc360d87e208
hhwang <wang@ispan.com.tw>
2022-04-21 10:26:24 +0800

    FirstAjax

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSIT133Site.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello Ajax", "text/plain");
+        }
+    }
+}
